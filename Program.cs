f60c9e1c37417ea01d584b68c7b1f67adeb307de
michwils <michwils@redhat.com>
2022-04-27 19:09:41 -0400

    added .env support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Kubernetes.Probes;
 
 //init
+DotNetEnv.Env.Load();
+
 var probes = new KubernetesProbes();
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,28 +29,4 @@ app.UseHttpsRedirection();
 //setup
 probes.ConfigureProbes(ref app);
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-  var forecast = Enumerable.Range(1, 5).Select(index =>
-     new WeatherForecast
-     (
-         DateTime.Now.AddDays(index),
-         Random.Shared.Next(-20, 55),
-         summaries[Random.Shared.Next(summaries.Length)]
-     ))
-      .ToArray();
-  return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- pkg/kubernetes/probes/probes.cs
+++ pkg/kubernetes/probes/probes.cs
@@ -5,19 +5,58 @@ namespace Kubernetes.Probes
   public class KubernetesProbes
   {
     private ProbeStatus _livnessStatus = new ProbeStatus();
+    private ProbeStatus _readinessStatus = new ProbeStatus();
+    private ProbeStatus _startupStatus = new ProbeStatus();
     public void ConfigureProbes(ref WebApplication app)
     {
       configureLiveness(ref app);
+      configureReadiness(ref app);
+      configureStartup(ref app);
     }
 
     private void configureLiveness(ref WebApplication app)
     {
-      app.MapGet("/healthz", async (c) =>
+      var path = DotNetEnv.Env.GetString("LIVENESS_PROBE");
+
+      if (path is not null)
       {
-        //TODO: Update this for future use change error to support status code, good enough for demo
-        c.Response.StatusCode = !_livnessStatus.Error ? 200 : 500;
-        await c.Response.WriteAsJsonAsync(_livnessStatus);
-      });
+        app.MapGet(path, async (c) =>
+        {
+          //TODO: Update this for future use change error to support status code, good enough for demo
+          c.Response.StatusCode = !_livnessStatus.Error ? 200 : 500;
+          await c.Response.WriteAsJsonAsync(_livnessStatus);
+        });
+      }
+    }
+
+    private void configureReadiness(ref WebApplication app)
+    {
+      var path = DotNetEnv.Env.GetString("READINESS_PROBE");
+
+      if (path is not null)
+      {
+        app.MapGet(path, async (c) =>
+        {
+          //TODO: Correct below
+          c.Response.StatusCode = 200;
+          await c.Response.WriteAsJsonAsync(_readinessStatus);
+        });
+      }
+    }
+
+    private void configureStartup(ref WebApplication app)
+    {
+      var path = DotNetEnv.Env.GetString("STARTUP_PROBE");
+
+      if (path is not null)
+      {
+        app.MapGet(path, async (c) =>
+        {
+          //TODO: Correct below
+          c.Response.StatusCode = 200;
+          await c.Response.WriteAsJsonAsync(_startupStatus);
+        });
+      }
     }
   }
 }
